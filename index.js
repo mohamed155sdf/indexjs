8b13b82f66b4adc3c178614d2b92f3e4d5dea1ce
mohamed155sdf <126342862+mohamed155sdf@users.noreply.github.com>
2023-02-25 16:56:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('url').innerHTML='<img   usemap="#image_map" src="https://i.ibb.co/jrM9Yd6/ezgif-com-gif-maker-4.webp"/>';
+  });
